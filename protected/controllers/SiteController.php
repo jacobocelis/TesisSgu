3595a5b8521a961b24def61be251cba25dfec3f7
Jacobo <Jacobo@pc>
2015-07-14 07:46:58 -0430

    arreglar formulas de disponiblidad y tiempos entre fallas y mejoras. terminar de agregar checkacces para permisos validar inputs entre otros.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -5,20 +5,20 @@ class SiteController extends Controller
 	/**
 	 * Declares class-based actions.
 	 */
-		public function filters()
+	/*	public function filters()
 	{
 		return array(
 			'accessControl', // perform access control for CRUD operations
 			'postOnly + delete', // we only allow deletion via POST request
 		);
-	}
+	}*/
 
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
 	 * @return array access control rules
 	 */
-	public function accessRules()
+	/*public function accessRules()
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
@@ -29,7 +29,7 @@ class SiteController extends Controller
 				'users'=>array('*'),
 			),
 		);
-	}
+	}*/
 	public function actions()
 	{
 		return array(
